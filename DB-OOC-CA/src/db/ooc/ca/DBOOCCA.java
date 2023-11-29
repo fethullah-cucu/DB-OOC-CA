050a10dad9238f01b427af9c0bb4e084534f0e6b
FethullahCucu <2022356@student.cct.ie>
2023-11-29 15:02:34 +0000

    hello
    from feto

--- DB-OOC-CA/src/db/ooc/ca/DBOOCCA.java
+++ DB-OOC-CA/src/db/ooc/ca/DBOOCCA.java
@@ -15,6 +15,7 @@ public class DBOOCCA {
      */
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!!!");
+        System.out.println("hello from feto");
     }
     
 }
